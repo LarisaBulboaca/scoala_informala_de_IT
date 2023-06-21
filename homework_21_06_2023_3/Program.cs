b232112790ea82507138750c614cb61078a5cb05
LarisaBulboaca <bulboaca.vl@gmail.com>
2023-06-21 22:44:05 +0300

    exercitiul 3

---
+++ homework_21_06_2023_3/Program.cs
@@ -0,0 +1,6 @@
+﻿//3. Write a program that reads from the console the radius "r" of a circle and prints its perimeter and area.
+Console.Write("Insert a radius value here: ");
+int r = int.Parse(Console.ReadLine());
+double perimeter = 2 * Math.PI * r;
+double area = Math.PI * r * r;
+Console.WriteLine($"The Perimeter of the circle with a radius of 20 is {perimeter} and the area of the circle is {area}");
