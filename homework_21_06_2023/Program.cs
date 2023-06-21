1839cb06e918f9b798b3bb31edf849bcc6af8075
LarisaBulboaca <bulboaca.vl@gmail.com>
2023-06-21 22:33:19 +0300

    exercitiu 1

---
+++ homework_21_06_2023/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Write a program that reads from the console three numbers of type int and prints their sum.
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter three numbers:");
+
+        Console.Write("Number 1 = ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Number 2 = ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Number 3 = ");
+        int number3 = int.Parse(Console.ReadLine());
+
+        int sum = number1 + number2 + number3;
+        Console.WriteLine("The sum of your three numbers is " + sum);
+    }
+}
